f2b402a6e72e191310ae59fb433b308c0716fd21
cindy <42073246+cxlhyphen@users.noreply.github.com>
2018-12-15 00:46:26 -0500

    completed challenge 1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,3 @@
-/*
-Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-The app should then prompt users with two messages.
-*/
-
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("dotenv").config();
@@ -16,75 +11,107 @@ const connection = mysql.createConnection({
     database: "bamazon"
 });
 
-//create connection to database w/ connect
-connection.connect(function(err) {
+//create connection to database w/ connect and run app
+connection.connect(function (err) {
     if (err) throw err;
     run();
 });
 
-//start application
-function run() {
-    //make query to display all items and info
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        let productArray = [];
-        for (let i = 0; i < res.length; i++) {
-            productArray.push(res[i]);
-        };
-        console.log(`\nWelcome to bamazon. Here are our current product listings:\n`);
-        for (let i = 0; i <productArray.length; i++){
-            console.log(`Item ID: ${productArray[i].item_id}`);
-            console.log(`Product Name: ${productArray[i].product_name}`);
-            console.log(`Price: $${productArray[i].price}`);
-            console.log("----------------------");
-        };
-        order();
-    });
-}
-
-function order() {
-    //inquirer: 1. ask for id they'd like to buy, 2. how many units they'd like to buy
+//functions
+let startOver = function() {
     inquirer.prompt([
         {
-            name: "id",
-            type: "input",
-            message: "What is the ID number of the item you're trying to purchase?",
-            validate: function(value) {
-                if (isNaN(value) === false) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        },
-        {
-            name: "quantity",
-            type: "input",
-            message: "How many would you like to purchase?",
-            validate: function(value) {
-                if (isNaN(value) === false) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+            type: "list",
+            name: "restart",
+            message: "Would you like to return to the main menu?",
+            choices: ["Yes", "No, exit."]
         }
-    ]).then(function(answer) {
-        //if your store has enough of the product to meet the customer's request, update sql database and reflect remainig quantify adn then show the total cost of their order
-        // Once the update goes through, show the customer the total cost of their purchase.
-        if (answer <= stock_quantity) {
-    
-            purchase();
-    
+    ]).then(function(restart) {
+        if (restart.restart === "No, exit.") {
+            process.exit();
         } else {
-            //else, doesn't run and log "Insufficient quantity"
-            console.log("Sorry, we currently do not have that many units.");
             run();
         }
     });
-    
-    }
+};
 
-function purchase() {
-    console.log("purchase can go thru");
+let run = function () {
+    //make query to display all items and info
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        console.log(`\nWelcome to bamazon. Here are our current product listings:\n`);
+
+        for (let i = 0; i < res.length; i++) {
+            console.log(`ID#: ${res[i].item_id} || Product Name: ${res[i].product_name} || Price: $${res[i].price}\n`);
+        };
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "choice",
+                message: "What would you like to do?",
+                choices: ["Purchase Items", "Exit"]
+            }
+        ]).then(function (choice) {
+            if (choice.choice === "Exit") {
+                process.exit();
+            }
+            order(res);
+        });
+    });
 }
+
+let order = function (res) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "id",
+            message: "What is the ID number of the item you're trying to purchase?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ]).then(function (answer) {
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].item_id == answer.id) {
+                let id = res[i].item_id;
+                let stock = res[i].stock_quantity;
+                let price = res[i].price;
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "quantity",
+                        message: "How many units would you like to purchase?",
+                        validate: function (value) {
+                            if (isNaN(value) === false) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+
+                    }
+                ]).then(function (answer) {
+                    if ((stock - answer.quantity) >= 0) {
+                        connection.query(
+                            "UPDATE products SET stock_quantity='" + (stock - answer.quantity) + "' WHERE item_id='" + id + "'",
+                            function (err) {
+                                if (err) throw err;
+                                console.log("Your order has been processed. Your total was: $"+(answer.quantity * price));
+                                startOver();
+                            });
+                    } else {
+                        console.log("Sorry, we currently do not have that many units.");
+                        startOver();
+                    }
+
+                });
+            }
+        };
+    });
+};
+
